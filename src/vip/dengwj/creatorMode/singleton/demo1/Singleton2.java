e782a97e37485099c7256978749d2766c65cd148
朴睦 <927561237@qq.com>
2024-08-27 22:47:53 +0800

    饿汉式：静态代码块

---
+++ src/vip/dengwj/creatorMode/singleton/demo1/Singleton2.java
@@ -0,0 +1,22 @@
+package vip.dengwj.creatorMode.singleton.demo1;
+
+/**
+ * @date 2024/8/27 22:45
+ * @author 朴睦
+ * @description 饿汉式：静态代码块
+ */
+public class Singleton2 {
+    private Singleton2() {
+
+    }
+
+    private static final Singleton2 instance;
+
+    static {
+        instance = new Singleton2();
+    }
+
+    public static Singleton2 getInstance() {
+        return instance;
+    }
+}
