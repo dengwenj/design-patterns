ae8be080d5643deb9040253b0630b6c1a8712154
朴睦 <927561237@qq.com>
2024-09-01 22:28:12 +0800

    Runtime

---
+++ src/vip/dengwj/creatorMode/singleton/demo1/Test4.java
@@ -0,0 +1,19 @@
+package vip.dengwj.creatorMode.singleton.demo1;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Test4 {
+    public static void main(String[] args) throws IOException {
+        Runtime runtime = Runtime.getRuntime();
+        // 调用 runtime 的方法 exec，参数要的是一个命令
+        Process process = runtime.exec("ifconfig");
+        // 获取输入流
+        InputStream is = process.getInputStream();
+        // 1KB = 1024bit，1M = 1024KB
+        byte[] buffer = new byte[1024 * 1024 * 10]; // 10M
+        // 读到的长度
+        int len = is.read(buffer);
+        System.out.println(new String(buffer, 0, len));
+    }
+}
