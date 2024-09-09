c9e89d83e81dae836ebbc3bc803966944cecf372
朴睦 <927561237@qq.com>
2024-09-09 17:26:14 +0800

    建造者模式 -> 模式扩展

---
+++ src/vip/dengwj/creatorMode/builder/Demo/Test.java
@@ -0,0 +1,20 @@
+package vip.dengwj.creatorMode.builder.Demo;
+
+public class Test {
+    public static void main(String[] args) {
+        Phone phone = new Phone.Builder()
+            .setBrand("华为")
+            .setColor("黑色")
+            .setMoney("6999")
+            .setWeight("1")
+            .build();
+        System.out.println(phone);
+
+        Phone phone1 = new Phone();
+        phone1.setBrand("小米");
+        phone1.setColor("白色");
+        phone1.setMoney("3999");
+        phone1.setWeight("1");
+        System.out.println(phone1);
+    }
+}
